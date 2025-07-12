e1bf7362a97029bca1897eba5f76e0df4ce54487
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2025-07-12 13:06:38 +0900

    chore(terraform): update workload identity provider in workflow and simplify outputs in Terraform configuration

--- google-cloud/mztn-seccamp2025/outputs.tf
+++ google-cloud/mztn-seccamp2025/outputs.tf
@@ -1,36 +1,4 @@
-# Outputs for mztn-seccamp2025
-
 output "workload_identity_provider" {
   description = "The full name of the Workload Identity Provider"
-  value       = google_iam_workload_identity_pool_provider.github_provider.name
-}
-
-output "github_actions_service_account_email" {
-  description = "Email of the GitHub Actions service account"
-  value       = google_service_account.github_actions.email
-}
-
-output "warren_service_account_email" {
-  description = "Email of the Warren service account"
-  value       = google_service_account.warren_runner.email
-}
-
-output "warren_cloud_run_url" {
-  description = "URL of the Warren Cloud Run service"
-  value       = length(google_cloud_run_v2_service.warren) > 0 ? google_cloud_run_v2_service.warren[0].uri : null
-}
-
-output "artifact_registry_repository" {
-  description = "Artifact Registry repository for container images"
-  value       = google_artifact_registry_repository.container_images.name
-}
-
-output "firestore_database_name" {
-  description = "Name of the Firestore database"
-  value       = google_firestore_database.warren_database.name
-}
-
-output "storage_bucket_name" {
-  description = "Name of the Cloud Storage bucket"
-  value       = google_storage_bucket.warren_bucket.name
+  value       = "projects/${data.google_project.this.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github_pool.workload_identity_pool_id}/providers/${google_iam_workload_identity_pool_provider.github_provider.workload_identity_pool_provider_id}"
 }
