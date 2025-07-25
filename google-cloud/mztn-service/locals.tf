0d1e3c9eeb366f5db45079f8f16063c9cc7de8dd
GitHub Action <action@github.com>
2025-07-25 19:15:18 +0000

    Update Warren image to sha256:3fc4d88fe7ca83a808e9a01de6b938803eee6440ec1ce2967b611dc864903245

--- google-cloud/mztn-service/locals.tf
+++ google-cloud/mztn-service/locals.tf
@@ -7,7 +7,7 @@ locals {
   github_repository = "m-mizutani/infra"
 
   # Warren configuration
-  warren_image_sha256 = "sha256:866ebc2b3948ef266220e7bd86444a8c41c013615af053500dc6cb164780678b"
+  warren_image_sha256 = "sha256:3fc4d88fe7ca83a808e9a01de6b938803eee6440ec1ce2967b611dc864903245"
   warren_image_uri    = "${local.region}-docker.pkg.dev/${local.project_id}/container-images/warren@${local.warren_image_sha256}"
 
   # Warren secrets
