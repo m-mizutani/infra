546f7625b46c7fd636bfd67f92873443968e7fd2
GitHub Action <action@github.com>
2025-09-06 03:44:25 +0000

    Update backstream image to sha256:9862bba9346af0299c52080e9b8ec30adde2daaf2987e1e2386ad1250c34ffe5

--- google-cloud/mztn-service/locals.tf
+++ google-cloud/mztn-service/locals.tf
@@ -22,6 +22,6 @@ locals {
   ]
 
   # Backstream configuration
-  backstream_image_sha256 = "sha256:406870943979f7ff837f1fc639df974ba39b2a051625beb0333306253d554774"
+  backstream_image_sha256 = "sha256:9862bba9346af0299c52080e9b8ec30adde2daaf2987e1e2386ad1250c34ffe5"
   backstream_image_uri    = "${local.region}-docker.pkg.dev/${local.project_id}/container-images/backstream@${local.backstream_image_sha256}"
 }
